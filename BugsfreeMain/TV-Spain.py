bfcaf33603ce5b81562be2ad9a79a72c5b41c025
pascgallardo <pascual.gallardo.ruiz@protonmail.com>
2026-04-24 21:17:43 +0200

    Actualizar TV-Spain.py

--- BugsfreeMain/TV-Spain.py
+++ BugsfreeMain/TV-Spain.py
@@ -261,8 +261,7 @@ def main():
         "https://m3u.work/qKChzP8.m3u",
         "https://m3u.work/YawDD3.m3u",
         "https://m3u.work/ICEQGPH.m3u",
-        "https://m3u.work/hJiHE3ad.m3u",        
-        "https://helmerluzo.github.io/SamsungTVPlus_HL/tv/m3u/SamsungTVPlus_tv_ES.m3u",
+        "https://m3u.work/hJiHE3ad.m3u",
     ]
 
     # Set check_links=False for super speed, True for accuracy
